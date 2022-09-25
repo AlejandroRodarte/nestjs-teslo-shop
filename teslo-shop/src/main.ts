2a7194577a0a6cc99b17191d86ec895dfc248ea7
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-09-24 22:45:42 -0600

    use process.env.PORT on bootstrap

--- teslo-shop/src/main.ts
+++ teslo-shop/src/main.ts
@@ -3,7 +3,8 @@ import './lib/docker/set-secrets';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = +process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
