3089b2f6fca8cc67a9b0750494ed62c44a8b44a8
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-09-25 19:34:46 -0600

    123. Create Product DTO

--- teslo-shop/src/main.ts
+++ teslo-shop/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import './lib/docker/set-secrets';
 import { AppModule } from './app.module';
 
@@ -6,6 +7,12 @@ async function bootstrap() {
   const port = +process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(port);
 }
 bootstrap();
--- teslo-shop/src/products/dto/create-product.dto.ts
+++ teslo-shop/src/products/dto/create-product.dto.ts
@@ -1,1 +1,47 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsEnum,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+import { ProductGender } from 'src/common/enums/product-gender.enum';
+import { ProductSize } from 'src/common/enums/product-size.enum';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  public title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  public price?: number;
+
+  @IsString()
+  @IsOptional()
+  public description?: string;
+
+  @IsString()
+  @IsOptional()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/)
+  public slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  public stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsEnum(ProductSize, { each: true })
+  public sizes: ProductSize[];
+
+  @IsString()
+  @IsEnum(ProductGender)
+  public gender: ProductGender;
+}
