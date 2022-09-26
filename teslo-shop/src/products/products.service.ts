1308fee5adc4ddb8e3fafbc06d1f24d684f250c1
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-09-25 20:32:58 -0600

    125. Manejo de errores

--- teslo-shop/src/products/products.service.ts
+++ teslo-shop/src/products/products.service.ts
@@ -1,10 +1,20 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { saveWrapper } from 'src/lib/async-wrappers/typeorm';
 import { QueryFailedError, Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+import {
+  UNIQUE_PRODUCT_TITLE_CONSTRAINT,
+  UNIQUE_PRODUCT_SLUG_CONSTRAINT,
+  POSITIVE_PRODUCT_PRICE_CONSTRAINT,
+  POSITIVE_OR_ZERO_PRODUCT_STOCK_CONSTRAINT,
+} from './entities/product.constraint-names';
 
 @Injectable()
 export class ProductsService {
@@ -19,8 +29,7 @@ export class ProductsService {
       repository: this.productRepository,
       entityLike: product,
     });
-    if (error && error instanceof QueryFailedError)
-      throw new InternalServerErrorException('Ayuda!');
+    if (error) this.handleError(error, product);
     return savedProduct;
   }
 
@@ -38,5 +47,38 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleError(error: Error, product: Product): void {
+    if (error instanceof QueryFailedError)
+      this.handleQueryFailedError(error, product);
+    throw new InternalServerErrorException(
+      'An error that is not of type QueryFailedError was thrown. Create a custom handler!',
+    );
+  }
+
+  private handleQueryFailedError(
+    error: QueryFailedError,
+    product: Product,
+  ): void {
+    const constraint = error.driverError.constraint as string;
+    throw new BadRequestException(
+      this.getConstraintMessage(constraint, product),
+    );
+  }
+
+  private getConstraintMessage(constraint: string, product: Product): string {
+    switch (constraint) {
+      case UNIQUE_PRODUCT_TITLE_CONSTRAINT:
+        return `There is already a product named '${product.title}' in the database`;
+      case UNIQUE_PRODUCT_SLUG_CONSTRAINT:
+        return `There is already a product with slug '${product.slug}' in the database`;
+      case POSITIVE_PRODUCT_PRICE_CONSTRAINT:
+        return 'Product price should be greater than $0.01';
+      case POSITIVE_OR_ZERO_PRODUCT_STOCK_CONSTRAINT:
+        return 'Product stock should be greater or equal than 0';
+      default:
+        return 'Unhandled constraint type. Create a custom message!';
+    }
   }
 }
