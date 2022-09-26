b97127533840f2fa1c15bc0981ed62932c3e8e05
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-09-25 19:20:53 -0600

    add class-validator and class-transformer

--- teslo-shop/src/main.ts
+++ teslo-shop/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = +process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(port);
 }
 bootstrap();
