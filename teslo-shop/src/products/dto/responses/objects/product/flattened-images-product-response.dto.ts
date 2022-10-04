55670bf66da18d51b6223f996131306cca79e2bc
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-04 15:52:24 -0600

    195. Expandir el ApiProperty

--- teslo-shop/src/products/dto/responses/objects/product/flattened-images-product-response.dto.ts
+++ teslo-shop/src/products/dto/responses/objects/product/flattened-images-product-response.dto.ts
@@ -3,36 +3,82 @@ import { ProductGender } from 'src/common/enums/product-gender.enum';
 import { ProductSize } from 'src/common/enums/product-size.enum';
 import { Product } from '../../../../entities/product.entity';
 import { ProductUserResponseDto } from '../user/product-user-response.dto';
+import { ProductType } from '../../../../../common/enums/product-type.enum';
 
 export class FlattenedImagesProductResponseDto {
-  @ApiProperty()
+  @ApiProperty({
+    example: '00869e4c-58b7-494e-aeff-805842cd300d',
+    description: 'Product UUID',
+    uniqueItems: true,
+  })
   public id: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Teslo T-Shirt',
+    description: 'Product Title',
+    uniqueItems: true,
+  })
   public title: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 10.5,
+    description: 'Product Price',
+    minimum: 0.01,
+  })
   public price: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    description: 'Product Description',
+    nullable: true,
+  })
   public description: string | null;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'teslo-t-shirt',
+    description: 'Product Slug (implemented for SEO routes)',
+    uniqueItems: true,
+  })
   public slug: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 10,
+    description: 'Product Stock',
+    default: 0,
+  })
   public stock: number;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: [ProductSize.S, ProductSize.L, ProductSize.XXL],
+    description: 'Product Available Sizes',
+    default: [],
+  })
   public sizes: ProductSize[];
 
-  @ApiProperty()
+  @ApiProperty({
+    example: ProductGender.UNISEX,
+    description: 'Product Gender',
+  })
   public gender: ProductGender;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: ['shirt', 'sweatshirt'],
+    description: 'Product Tags',
+    default: [],
+  })
   public tags: string[];
 
-  @ApiProperty()
+  @ApiProperty({
+    example: ProductType.HOODIES,
+    description: 'Product Type',
+  })
+  public type: ProductType;
+
+  @ApiProperty({
+    example: ['1740176-00-A_0_2000.jpg', '1740176-00-A_1.jpg'],
+    description: 'Product Image Filenames',
+    default: [],
+  })
   public images: string[];
 
   @ApiProperty()
@@ -48,6 +94,7 @@ export class FlattenedImagesProductResponseDto {
     sizes: ProductSize[],
     gender: ProductGender,
     tags: string[],
+    type: ProductType,
     images: string[],
     user: ProductUserResponseDto,
   ) {
@@ -60,6 +107,7 @@ export class FlattenedImagesProductResponseDto {
     this.sizes = sizes;
     this.gender = gender;
     this.tags = tags;
+    this.type = type;
     this.images = images;
     this.user = user;
   }
@@ -77,6 +125,7 @@ export class FlattenedImagesProductResponseDto {
       productEntity.sizes,
       productEntity.gender,
       productEntity.tags,
+      productEntity.type,
       productEntity.images.map((productImage) => productImage.url),
       new ProductUserResponseDto(
         productEntity.user.id,
--- teslo-shop/src/products/dto/responses/objects/user/product-user-response.dto.ts
+++ teslo-shop/src/products/dto/responses/objects/user/product-user-response.dto.ts
@@ -1,13 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ProductUserResponseDto {
-  @ApiProperty()
+  @ApiProperty({
+    example: '292052d3-aae0-4315-8af1-6ec4ff336c97',
+    description: 'User UUID',
+    uniqueItems: true,
+  })
   public id: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'example@gmail.com',
+    description: 'User E-Mail',
+    uniqueItems: true,
+  })
   public email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'John Doe',
+    description: "User's Full Name",
+  })
   public fullName: string;
 
   constructor(id: string, email: string, fullName: string) {
