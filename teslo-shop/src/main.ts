5eec7cd89034bdfae5f49102a0a2ae4c113ac68f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-03 21:20:33 -0600

    193. Nestjs swagger - OpenAPI Specification

--- teslo-shop/src/main.ts
+++ teslo-shop/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import './common/init/docker/set-secrets.init';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const port = +process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,6 +15,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo-Shop RESTful API')
+    .setDescription('Test-Shop endpoints')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();
