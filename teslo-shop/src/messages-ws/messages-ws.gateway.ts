4572847c2d045bbd25ec6ac9aa00c973694f7a38
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-05 14:14:44 -0600

    203. Server - Escuchar conexiones y desconexiones

--- teslo-shop/src/messages-ws/messages-ws.gateway.ts
+++ teslo-shop/src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,27 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { DefaultEventsMap } from 'socket.io/dist/typed-events';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+export class MessagesWsGateway
+  implements OnGatewayConnection<Socket>, OnGatewayDisconnect<Socket>
+{
   constructor(private readonly messagesWsService: MessagesWsService) {}
+  handleConnection(
+    client: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
+    ...args: any[]
+  ) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(
+    client: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
+  ) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
 }
