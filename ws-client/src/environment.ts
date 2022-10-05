51cb97257c32b0dc863844ff69a8e9e375f6ab75
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2022-10-05 16:48:25 -0600

    204b. Cliente - Vite Vanilla TypeScript

---
+++ ws-client/src/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  serverApi: import.meta.env.VITE_APP_SERVER_API,
+};
---
+++ ws-client/src/sockets/connect-to-server.ts
@@ -0,0 +1,9 @@
+import { Manager } from 'socket.io-client';
+import { environment } from '../environment';
+
+export const connectToServer = () => {
+  const endpoint = `${environment.serverApi}/socket.io/socket.io.js`;
+  const manager = new Manager(endpoint);
+
+  const socket = manager.socket('/');
+};
